6730e61c9343c8a5dfa1d6d8d11afeec524251f6
JIHYE JANG <jj749@cornell.edu>
2023-02-17 15:04:18 -0500

    q1 change

--- 01-ps-arnold-jang.jl
+++ 01-ps-arnold-jang.jl
@@ -5,7 +5,7 @@
 #import Pkg
 using Pkg
 Pkg.add("Distributions")
-Pkg.add("QuadGK")
+Pkg.add("QuadGK") # use  Gauss-Kronrod quadrature
 Pkg.add("Optim")
 Pkg.add("CompEcon")
 
@@ -19,6 +19,12 @@ Pkg.add("CompEcon")
 #and a cost function:
 ## C(q) = cq
 
+import Pkg; Pkg.add("Distributions")
+import Pkg; Pkg.add("QuadGK")
+import Pkg; Pkg.add("Optim")
+import Random
+Random.seed!(1234)
+
 using Distributions, QuadGK, Optim
 
 function profit_max_q(a, c, mu, sigma, method, n)
@@ -29,33 +35,31 @@ function profit_max_q(a, c, mu, sigma, method, n)
         return a - b*q
     end
 
-    # Define profit function for Monte Carlo method
+    # Define profit function
     function profit(q)
         rev = P(q)*q
         cost = c*q
         return rev - cost
     end
 
-    # Define expected profit function for quadrature method
+    # Define expected profit function
     function expected_profit(q)
         b_dist = LogNormal(mu, sigma)
         b_pdf(x) = pdf(b_dist, x)
         b_cdf(x) = cdf(b_dist, x)
-        # first term: integral ([1]) for when quantity smaller than q, expected revenue from all the units sold up to the quantity q
-        # second term: expected revenue from any remaining demand that is not satisfied by the quantity q
         rev = quadgk(x -> P(x)*x*b_pdf(x), -Inf, q)[1] + (a - P(q))*q* b_cdf(q)
         cost = c*q
         return rev - cost
     end
 
     if method == "mc"
-        # Monte Carlo integration using profit function 
-        q_draws = rand(LogNormal(mu, sigma), n)
-        profits = [profit(q) for q in q_draws]
+        # Monte Carlo integration
+        q_vals = rand(LogNormal(mu, sigma), n)
+        profits = [profit(q) for q in q_vals]
         optimal_q = q_vals[argmax(profits)]
     elseif method == "quad"
-        # Gaussian quadrature integration using expected profit function
-        result = optimize(expected_profit, 0.0, 1.0)
+        # Gaussian quadrature integration
+        result = optimize(expected_profit, 0.0, 1000)
         optimal_q = result.minimizer
     else
         error("Invalid method choice. Choose 'mc' or 'quad'.")
@@ -64,14 +68,13 @@ function profit_max_q(a, c, mu, sigma, method, n)
     return optimal_q
 end
 
-
 #part 2
 #solve the profit_max_q function with a set of values 
 # 1. solve with the Monte Carlo method
-profit_max_q(500, 20, 5, 7,"mc", 1000)
+profit_max_q(500, 20, 5, 0.1,"mc", 1000)
 
 # 2. solve with the Quadrature method
-profit_max_q(500, 20, 5, 7,"quad", 1000)
+profit_max_q(500, 20, 5, 0.1,"quad", 1000)
 
 #part 3
 #Make sure your code is type-stable by using the code introspection macros (e.g. @code_llvm, @code_warntype, @trace)
