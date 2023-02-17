38d500791aad1270b49b7d2b1a438012b17644e7
dean <trevor.d.arnold@gmail.com>
2023-02-16 23:12:39 -0500

    solved problem 2

--- 01-ps-arnold-jang.jl
+++ 01-ps-arnold-jang.jl
@@ -5,6 +5,10 @@
 using Pkg
 Pkg.add("CompEcon")
 
+#---------------------------------------------------------#
+#Problem 1
+#---------------------------------------------------------#
+
 #part 1
 #a profit max firm faces the following demand curve:
 ## P(q) = a-bq
@@ -26,6 +30,32 @@ function profit_max_q(a, c, mu, sigma, method, n)
 
 
 
+
+        function profit_max_q(a, c, mu, sigma, method, n)
+            if method == "mc"
+                # Generate n random draws from a lognormal distribution
+                draws = rand(LogNormal(mu, sigma), n)
+                # Compute profits for each draw
+                prices = a - draws
+                quantities = min(prices / (2c), draws / 2)
+                profits = quantities .* (prices - c * quantities)
+                # Estimate the expected profit as the mean of the profits
+                return mean(profits)
+            elseif method == "quad"
+                # Define the integrand function for computing the expected profit
+                integrand(x) = begin
+                    q = min((a - exp(x)) / (2c), exp(x) / 2)
+                    (a - exp(x)) * q - c * q^2 * exp(x) * pdf(LogNormal(mu, sigma), x)
+                end
+                # Compute the expected profit using quadrature integration
+                nodes, weights = qnwnorm(n, mu, sigma^2)
+                return quad(integrand, -Inf, Inf, nodes, weights)
+            else
+                error("Invalid method: $method")
+            end
+        end
+               
+
 #part 2
 #solve the profit_max_q function with a set of values 
 
@@ -34,3 +64,22 @@ function profit_max_q(a, c, mu, sigma, method, n)
 
 
 #solve with the Quadrature method
+
+
+
+#---------------------------------------------------------#
+#Problem 2: Monte Carlo Integration
+#---------------------------------------------------------#
+
+function monte_carlo_pi(n::Int)
+    inside_circle = 0
+    for i in 1:n
+        x, y = rand(-1:1, 2)
+        if x^2 + y^2 <= 1
+            inside_circle += 1
+        end
+    end
+    return 4 * inside_circle / n
+end
+
+monte_carlo_pi(10^4)
