76051ccea5624a934d027f62d2ea622d12ecb084
dean <trevor.d.arnold@gmail.com>
2023-02-17 22:25:51 -0500

    added solution to and data for problem 3

--- 01-ps-arnold-jang.jl
+++ 01-ps-arnold-jang.jl
@@ -92,3 +92,19 @@ function monte_carlo_pi(n::Int)
 end
 
 monte_carlo_pi(10^4)
+
+
+
+#---------------------------------------------------------#
+#Problem 3: Shell scripting
+#---------------------------------------------------------#
+
+#!/bin/sh
+for i in {1..50}; do touch file-$i.txt; done
+for i in {1..50}; do sed -n "${i}p" adult.data.txt >> file-${i}.txt; done
+find . -depth -name '*-*' -execdir sh -c 'mv "$1" "$(echo "$1" | sed s/-/_/g)"' _ {} \;
+find -depth -name '*-*' -execdir sh -c 'mv "$1" "$(echo "$1" | tr "-" "_")"' _ {} \;
+cut -d ',' -f 10 new_data_set.csv | grep -c -i "Male" > output.txt
+cut -d ',' -f 7 new_data_set.csv | sort -u | awk 'END {print NR}' >> output.txt
+find . -type f ! -name 'output.txt' ! -name 'adult.data.txt' -delete
+    
